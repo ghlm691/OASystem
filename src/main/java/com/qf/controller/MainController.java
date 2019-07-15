dbdd21be1e9872793692dd4d374f5f521532c932
jerry <15726455210@163.com>
2019-07-15 17:49:35 +0800

    导入登录界面

--- src/main/java/com/qf/controller/MainController.java
+++ src/main/java/com/qf/controller/MainController.java
@@ -1,12 +1,19 @@
 package com.qf.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * author:赖文熙
  * date:2019/7/15 12:01
  * description:主控制器
  */
 
-
+@Controller
 public class MainController {
 
+    @RequestMapping("index")
+    public String goIndex() {
+        return "login";
+    }
 }
